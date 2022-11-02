c90d49426029ce52d7d84a0920b57ef28789ab11
Manuelzimmermann <zimmermannm@udesa.edu.ar>
2022-11-02 13:05:45 -0300

    funcion 1 terminada

--- tp2.py
+++ tp2.py
@@ -1,16 +1,32 @@
 def read_file(archi):
+    '''
+    Esta funcion devuelve un diccionario con los valores del archivo bolsa.csv La key
+    '''
     dic_bolsa = {}
+    dic_bolsa2={}
     with open(archi, 'r', encoding = 'utf-8') as file:
-        for elementos in file:
-            h, a, o, i, l = elementos.strip('\n').split(',')
-            dic_bolsa[h] = a, o, i, l
-            print(dic_bolsa)
+        titulos = file.readline().strip().split(',')
+        for i in titulos:
+            dic_bolsa[i] = []
+        for x in file:
+            info = x.strip().split(',')
+            for key, val in zip(titulos, info):
+                dic_bolsa[key].append(val)
+    return dic_bolsa
 
 diccionario = read_file('bolsa.csv')   
-archi = 'bolsa.csv'
 print(diccionario)
+def monthly(accion, diccionario):
+    
 
-def monthly_average(accion = 'SATL', diccionario):
- fechas, promedios_mes = monthly_average("SATL", diccionario)
+# def monthly_average(diccionario, accion):
+#     for x in diccionario:
+#         if
 
+
+#  fechas, promedios_mes = monthly_average("SATL", diccionario)
+
+
+# QUE tinee que devolver el dic 
+# Como uso lo que ponen en el tp
  
