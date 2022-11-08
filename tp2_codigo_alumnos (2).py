9e5c6e52c17c18f625889b49f05f49672afca77d
Manuelzimmermann <zimmermannm@udesa.edu.ar>
2022-11-08 20:10:45 -0300

    kak a

--- tp2_codigo_alumnos (2).py
+++ tp2_codigo_alumnos (2).py
@@ -79,37 +79,82 @@ def read_file(archi):
 
 diccionario = read_file('bolsa.csv')   
 # print(diccionario)
-lista = []
-meses =[]
+lista12 = []
 lista11 = []
+lista10 = []
+lista9 = []
+lista8 = []
+lista7 = []
+lista6 = []
+lista5 = []
+lista4 = []
+lista3 = []
+lista2 = []
+lista1 = []
+
+meses =[]
+
 def monthly(accion, diccionario):
  lista_fechas = str2datetime(diccionario["Date"])
  for index, fechas in enumerate(lista_fechas):
     #  print(index, fechas)
      mes = fechas.month
-     for x in str(mes):
-         meses_totales = meses.append(x)
-         print(meses_totales)
-    
-    #  if mes == 10:
-    #     lista.append(diccionario[accion][index])
-    #     promedio = sum(lista)/len(lista)
-    #     print(promedio)
-    #  elif mes == 11:
-    #         lista11.append(diccionario[accion][index])
-    #         promedio11 = sum(lista11)/len(lista11)
-    #         return promedio11
-    #  elif mes == 12:
-    #         lista11.append(diccionario[accion][index])
-    #         promedio11 = sum(lista11)/len(lista11)
-    #         return promedio11
+     if mes == 10:
+        lista10.append(diccionario[accion][index])
+        promedio10 = sum(lista10)/len(lista10)
+        return promedio10, fechas
+        
+     elif mes == 11:
+            lista11.append(diccionario[accion][index])
+            promedio11 = sum(lista11)/len(lista11)
+            return promedio11, fechas
+     elif mes == 12:
+            lista12.append(diccionario[accion][index])
+            promedio12 = sum(lista12)/len(lista12)
+            return promedio12, fechas
+     elif mes == 9:
+        lista9.append(diccionario[accion][index])
+        promedio9 = sum(lista9)/len(lista9)
+        return promedio10, fechas
+     elif mes == 8:
+            lista8.append(diccionario[accion][index])
+            promedio8 = sum(lista8)/len(lista8)
+            return promedio8, fechas
+     elif mes == 7:
+            lista12.append(diccionario[accion][index])
+            promedio7 = sum(lista7)/len(lista7)
+            return promedio7, fechas
+     elif mes == 6:
+        lista6.append(diccionario[accion][index])
+        promedio6 = sum(lista6)/len(lista6)
+        return promedio6, fechas
+     elif mes == 5:
+            lista5.append(diccionario[accion][index])
+            promedio5 = sum(lista5)/len(lista5)
+            return promedio5, fechas
+     elif mes == 4:
+            lista4.append(diccionario[accion][index])
+            promedio4 = sum(lista4)/len(lista4)
+            return promedio4, fechas
+     elif mes == 3:
+        lista3.append(diccionario[accion][index])
+        promedio3 = sum(lista3)/len(lista3)
+        return promedio3, fechas
+     elif mes == 2:
+            lista2.append(diccionario[accion][index])
+            promedio2 = sum(lista2)/len(lista2)
+            return promedio2, fechas
+     elif mes == 1:
+            lista1.append(diccionario[accion][index])
+            promedio1 = sum(lista1)/len(lista1)
+            return promedio1, fechas
 
-
-    
 
 accion = "SATL"
 print(monthly(accion, diccionario))
 
+with open('monthly_average_SATL.csv', 'r', encoding = 'utf-8') as file:
+    file.write(monthly(accion, diccionario))
 
 
 
